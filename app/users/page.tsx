68765926774151cf975fafc2f65f5e5bf4483dab
kadunapikin <ibakoshi27@gmail.com>
2023-11-18 07:45:32 +0100

    map through users array and return an unodered list of users in the jsx

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -10,6 +10,10 @@ const UsersPage = async () => {
     const users: User[] = await res.json();
   return (
     <>
+    <h1>Users</h1>
+    <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
     </>
   )
 }
