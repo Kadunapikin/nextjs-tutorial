70a7459d4dcf5d703ac4905e1ce70395005b5333
kadunapikin <ibakoshi27@gmail.com>
2023-11-18 07:15:32 +0100

    create a components dir, add a ProductCard.tsx file and add AddToCart btn as a child client component to ProductCart.jsx

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+// 'use client'
+import React from 'react'
+import AddToCard from './AddToCard'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <h2>Products Cart</h2>
+        <AddToCard />
+    </div>
+  )
+}
+
+export default ProductCard
